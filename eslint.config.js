3cbf9e50e09a582fb34b381ea60afdceb78910cd
Maria-Fontica Marinescu <mariafmarinescu@outlook.com>
2024-10-07 18:05:27 +0200

    Install & configure eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,28 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import react from 'eslint-plugin-react';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    ignores: ['node_modules'],
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*.js', '**/*.jsx'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      sourceType: 'module',
+      ecmaVersion: 'latest',
+      globals: {
+        React: 'writable',
+      },
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      react,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'react/react-in-jsx-scope': 'off', // React 17+ JSX doesn't require React import
+      'react/prop-types': 'off', // Disable PropTypes if you're using TypeScript or prefer not using them
     },
   },
-)
+  prettier,
+];
