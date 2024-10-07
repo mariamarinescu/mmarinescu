e7f9694de9119dfb2a4ad41b68e3f9cc37118b02
Maria-Fontica Marinescu <mariafmarinescu@outlook.com>
2024-10-07 21:55:59 +0200

    Implement DarkModeButton as toggle button & use

--- src/components/buttons/DarkModeButton.tsx
+++ src/components/buttons/DarkModeButton.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@headlessui/react';
+import clsx from 'clsx';
 import { useState } from 'react';
 import { MoonIcon, SunIcon } from '../icons';
 
@@ -7,14 +8,37 @@ export const DarkModeButton = () => {
 
   const darkModeHandler = () => {
     setDark(!dark);
-    document.body.classList.toggle('dark');
+    return !dark
+      ? document.documentElement.classList.add('dark')
+      : document.documentElement.classList.remove('dark');
   };
+
   return (
     <Button
-      onClick={() => darkModeHandler()}
-      className="absolute top-4 right-4 p-5 bg-gray-200 dark:bg-gray-800 rounded-full shadow-lg"
+      className="absolute top-4 right-4 w-16 h-8 flex items-center cursor-pointer"
+      onClick={darkModeHandler}
     >
-      {dark ? <SunIcon /> : <MoonIcon />}
+      {/* Toggle switch background*/}
+      <div
+        className={clsx(
+          'absolute w-full h-full rounded-full transition-colors duration-300',
+          !dark ? 'bg-gray-800' : 'bg-gray-200'
+        )}
+      />
+
+      {/* Toggle button */}
+      <div
+        className={clsx(
+          'absolute w-8 h-8 rounded-full shadow-md transition-transform duration-300',
+          !dark ? 'translate-x-8 bg-gray-600' : 'bg-white'
+        )}
+      >
+        {dark ? (
+          <SunIcon className="text-black w-5 h-5 absolute top-1.5 left-1.5" />
+        ) : (
+          <MoonIcon className="text-white w-5 h-5 absolute top-1.5 left-1.5" />
+        )}
+      </div>
     </Button>
   );
 };
