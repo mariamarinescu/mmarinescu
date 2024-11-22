7a6c74ae1d4050627511f570db6505e58d46489e
Maria Marinescu <mariafmarinescu@outlook.com>
2024-11-22 11:42:45 +0100

    Increase the height of the footer; increase footer font-size

--- src/modules/app/components/Footer.tsx
+++ src/modules/app/components/Footer.tsx
@@ -12,11 +12,11 @@ export const Footer: React.FC<FooterProps> = ({ className }: FooterProps) => {
     <footer
       className={clsx(
         className,
-        'bg-white dark:bg-th-darker-gray text-center w-full h-auto sm:h-6 xl:h-10 flex items-center bottom-0 absolute'
+        'bg-white dark:bg-th-darker-gray text-center w-full h-auto sm:h-10 xl:h-16 flex items-center bottom-0 absolute'
       )}
     >
       <div className="mx-auto h-auto">
-        <p className="text-[8px] text-xs xl:text-md text-gray-800 dark:text-th-medium-light-gray-1">
+        <p className="text-[8px] text-xs md:text-md xl:text-xl text-gray-800 dark:text-th-medium-light-gray-1">
           &copy; {currentYear} Maria-Fontica Marinescu. All rights reserved.
         </p>
       </div>
