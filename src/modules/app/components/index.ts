8113884f6e96642629a9d11db378088fd66afb9b
Maria Marinescu <mariafmarinescu@outlook.com>
2024-12-01 16:43:27 +0100

    Delete LinkedInShareButton

--- src/modules/app/components/index.ts
+++ src/modules/app/components/index.ts
@@ -1,5 +1,4 @@
 export { DarkModeButton } from './DarkModeButton';
 export { Footer } from './Footer';
-export { LinkedInShareButton } from './LinkedInShareButton';
 export { ResumeDownloadButton } from './ResumeDownloadButton';
 export { TopBar } from './TopBar';
