bcfccff76ccb5c625ba6ba9925174a4692f09176
Maria Marinescu <mariafmarinescu@outlook.com>
2024-12-03 18:48:25 +0100

    Add errorContainer to input style hook

--- src/components/inputs/useInputStyle.ts
+++ src/components/inputs/useInputStyle.ts
@@ -4,16 +4,18 @@ export interface InputStyle {
   label: string;
   errorMessage: string;
   container: string;
+  errorContainer: string;
 }
 
 const useInputStyle = (customStyle?: Partial<InputStyle>): InputStyle => {
   const defaultStyle: InputStyle = {
     container: 'flex flex-col gap-0.5 p-2',
     base: 'border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring-1 focus:ring-neon-purple',
-    error: 'border-red-500',
     label:
-      'block text-sm sm:text-base md:text-md xl:text-lg text-gray-700 dark:text-gray-200',
+      'block text-sm md:text-base xl:text-md flex gap-0.7 text-gray-700 dark:text-gray-200',
+    error: 'border-red-500',
     errorMessage: 'text-red-500 text-xs sm:text-sm xl:text-md',
+    errorContainer: 'h-2 flex flex-col items-start content-end',
   };
 
   return { ...defaultStyle, ...customStyle };
