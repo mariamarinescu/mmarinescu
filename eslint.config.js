9d32a7e3c7fdb91111b1cba871ce0f65b815de34
Maria Marinescu <mariafmarinescu@outlook.com>
2024-12-03 12:35:52 +0100

    Change eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,13 @@
 import js from '@eslint/js';
 import prettier from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
 import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
 
 export default [
   {
-    ignores: ['node_modules'],
+    ignores: ['node_modules', 'dist', 'build'],
   },
   js.configs.recommended,
   {
@@ -16,10 +19,43 @@ export default [
         React: 'writable',
       },
     },
-    plugins: { react },
+    plugins: { react, reactHooks, importPlugin, jsxA11y },
     rules: {
-      'react/react-in-jsx-scope': 'off', // React 17+ JSX doesn't require React import
-      'react/prop-types': 'off', // Disable PropTypes if you're using TypeScript or prefer not using them
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+
+      'react/jsx-key': 'warn',
+      'react/no-deprecated': 'warn',
+      'react/no-unknown-property': 'warn',
+      'react/self-closing-comp': 'warn',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+
+      'import/order': [
+        'warn',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+          ],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+      'import/no-unresolved': 'error',
+      'import/no-duplicates': 'warn',
+
+      'jsx-a11y/alt-text': 'warn',
+      'jsx-a11y/anchor-is-valid': 'warn',
+      'jsx-a11y/click-events-have-key-events': 'warn',
+      'jsx-a11y/no-static-element-interactions': 'warn',
     },
   },
   prettier,
