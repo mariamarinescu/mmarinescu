a8795cf8d18e9bf52adb8264502102e4fb01cc34
Maria Marinescu <mariafmarinescu@outlook.com>
2024-12-03 10:57:43 +0100

    Add tailwind.config plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,5 +21,9 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 };
