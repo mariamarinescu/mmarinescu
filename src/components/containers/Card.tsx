d79bc808e62fd1044dd0ccdb9a38cdb08055f26d
Maria Marinescu <mariafmarinescu@outlook.com>
2024-12-06 18:48:07 +0100

    Add title to card

--- src/components/containers/Card.tsx
+++ src/components/containers/Card.tsx
@@ -2,17 +2,24 @@ import clsx from 'clsx';
 
 interface CardProps {
   className?: string;
+  title?: string;
   children: React.ReactNode;
 }
 
-const Card: React.FC<CardProps> = ({ className, children }) => {
+const Card: React.FC<CardProps> = ({ className, title, children }) => {
   return (
     <div
       className={clsx(
         className,
-        'rounded-lg border border-gray-light-muted bg-white p-4 dark:bg-gray-light-muted lg:p-8'
+        !!title && 'flex flex-col gap-4',
+        'rounded-lg border border-gray-light-muted bg-white p-4 dark:bg-gray-light-muted xs:p-5 lg:p-8'
       )}
     >
+      {title && (
+        <h6 className="font-mono text-lg text-black dark:text-white">
+          {title}
+        </h6>
+      )}
       {children}
     </div>
   );
