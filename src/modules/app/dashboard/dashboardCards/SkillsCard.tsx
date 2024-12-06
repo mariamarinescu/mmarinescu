a7e8e52fbeb5ae32e3eeb19dc99a7ccc7f089bb9
Maria Marinescu <mariafmarinescu@outlook.com>
2024-12-06 22:20:38 +0100

    Improve SkillsCard component

--- src/modules/app/dashboard/dashboardCards/SkillsCard.tsx
+++ src/modules/app/dashboard/dashboardCards/SkillsCard.tsx
@@ -12,60 +12,73 @@ import {
 } from './icons';
 import { RecoilIcon } from './icons/skillsCard/RecoilIcon';
 
-export const SkillsCard = ({ className }: { className?: string }) => {
+const iconClassName = 'h-8 w-8 md:h-12 md:w-12';
+
+const buttonListConfig = [
+  {
+    dataTooltipContent: 'Javascript',
+    dataTooltipId: 'my-js-tooltip',
+    icon: <JSIcon className={iconClassName} />,
+  },
+  {
+    dataTooltipContent: 'Typescript',
+    dataTooltipId: 'my-typescript-tooltip',
+    icon: <TypescriptIcon className={iconClassName} />,
+  },
+  {
+    dataTooltipContent: 'React.js',
+    dataTooltipId: 'my-react-tooltip',
+    icon: <ReactIcon className={iconClassName} />,
+  },
+  {
+    dataTooltipContent: 'HTML5',
+    dataTooltipId: 'my-html-tooltip',
+    icon: <HTML5Icon className={iconClassName} />,
+  },
+  {
+    dataTooltipContent: 'CSS3',
+    dataTooltipId: 'my-css-tooltip',
+    icon: <CSS3Icon className={iconClassName} />,
+  },
+  {
+    dataTooltipContent: 'Tailwind',
+    dataTooltipId: 'my-tailwind-tooltip',
+    icon: <TailwindCSSIcon className={iconClassName} />,
+    href: 'https://tailwindcss.com',
+  },
+  {
+    dataTooltipContent: 'Recoil.js',
+    dataTooltipId: 'my-recoil-tooltip',
+    icon: <RecoilIcon className={iconClassName} />,
+    href: 'https://recoiljs.org',
+  },
+  {
+    dataTooltipContent: 'Redux.js',
+    dataTooltipId: 'my-redux-tooltip',
+    icon: <ReduxIcon className={iconClassName} />,
+    href: 'https://redux.js.org',
+  },
+];
+
+interface SkillsCardProps {
+  className?: string;
+}
+
+export const SkillsCard: React.FC<SkillsCardProps> = ({ className }) => {
   return (
     <Card title="What can I do?" className={clsx('h-full w-full', className)}>
       <div className="align-center flex h-fit w-full flex-wrap items-center justify-start gap-2">
-        <ButtonWithTooltip
-          dataTooltipContent="Javascript"
-          dataTooltipId="my-js-tooltip"
-          icon={<JSIcon className="h-8 md:w-8" />}
-        />
-
-        <ButtonWithTooltip
-          dataTooltipContent="Typescript"
-          dataTooltipId="typescript-icon"
-          icon={<TypescriptIcon className="h-8 w-8" />}
-        />
-
-        <ButtonWithTooltip
-          dataTooltipContent="React.js"
-          dataTooltipId="my-tooltip"
-          icon={<ReactIcon className="h-8 w-8" />}
-        />
-
-        <ButtonWithTooltip
-          dataTooltipContent="HTML5"
-          dataTooltipId="my-html-tooltip"
-          icon={<HTML5Icon className="h-8 w-8" />}
-        />
-
-        <ButtonWithTooltip
-          dataTooltipContent="CSS3"
-          dataTooltipId="my-css-tooltip"
-          icon={<CSS3Icon className="h-8 w-8" />}
-        />
-
-        <ButtonWithTooltip
-          dataTooltipContent="Tailwind"
-          dataTooltipId="my-tailwind-tooltip"
-          icon={<TailwindCSSIcon className="h-8 w-8" />}
-          href="https://tailwindcss.com"
-        />
-
-        <ButtonWithTooltip
-          dataTooltipContent="Recoil.js"
-          dataTooltipId="my-recoil-tooltip"
-          icon={<RecoilIcon className="h-8 w-8" />}
-          href="https://recoiljs.org"
-        />
-
-        <ButtonWithTooltip
-          dataTooltipContent="Redux.js"
-          dataTooltipId="my-redux-tooltip"
-          icon={<ReduxIcon className="h-8 w-8" />}
-          href="https://redux.js.org"
-        />
+        {buttonListConfig.map(
+          ({ dataTooltipContent, dataTooltipId, icon, href }) => (
+            <ButtonWithTooltip
+              key={dataTooltipId}
+              dataTooltipContent={dataTooltipContent}
+              dataTooltipId={dataTooltipId}
+              icon={icon}
+              href={href}
+            />
+          )
+        )}
       </div>
     </Card>
   );
