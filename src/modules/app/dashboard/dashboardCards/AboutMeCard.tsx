611393d0157e9ae36b23248283a3ee10f32fa876
Maria Marinescu <mariafmarinescu@outlook.com>
2024-12-06 23:12:40 +0100

    Change img on AboutMeCard

--- src/modules/app/dashboard/dashboardCards/AboutMeCard.tsx
+++ src/modules/app/dashboard/dashboardCards/AboutMeCard.tsx
@@ -1,7 +1,6 @@
 import clsx from 'clsx';
 import { Card } from 'components/containers';
 import React from 'react';
-import { Woman } from './icons';
 
 interface AboutMeCardProps {
   className?: string;
@@ -11,10 +10,14 @@ export const AboutMeCard: React.FC<AboutMeCardProps> = ({ className }) => {
   return (
     <Card className={clsx('p-4', className)} title="Who am I?">
       <div>
-        <div className="float-right mb-1 ml-2 mr-2">
-          <Woman className="float-right h-40 w-40" />
+        <div className="float-right mb-1 ml-2 mr-2 w-4/12">
+          <img
+            src="public/img/mountain-6783202_1280.png"
+            width="100%"
+            height="100%"
+          />
         </div>
-        <p className="text-md text-justify font-mono text-gray-700 dark:text-gray-300 md:text-lg">
+        <p className="main-text-size prose text-justify font-serif text-gray-700 lg:prose-xl dark:text-gray-300">
           Away from my keyboard, I love connecting with nature on sunny days, at
           the mountain or at the beach. On rainy days, I swap code for recipes,
           cooking for loved ones and creating connections around the table. I
