1835d04095f61db30d2afd312eb2ccfe521fd821
Maria Marinescu <mariafmarinescu@outlook.com>
2024-12-07 01:31:26 +0100

    Change photo on AboutMeCard

--- src/modules/app/dashboard/dashboardCards/AboutMeCard.tsx
+++ src/modules/app/dashboard/dashboardCards/AboutMeCard.tsx
@@ -8,20 +8,24 @@ interface AboutMeCardProps {
 
 export const AboutMeCard: React.FC<AboutMeCardProps> = ({ className }) => {
   return (
-    <Card className={clsx('p-4', className)} title="Who am I?">
+    <Card className={clsx(className)} title="Who am I?">
       <div>
-        <div className="float-right mb-1 ml-2 mr-2 w-4/12">
-          <img
-            src="public/img/mountain-6783202_1280.png"
-            width="100%"
-            height="100%"
-          />
+        <div className="float-right mb-1 ml-6 w-2/12">
+          <div className="h-full w-full pt-2">
+            <img
+              src="public/img/me.jpg"
+              width="100%"
+              height="100%"
+              className="rounded-lg object-contain"
+            />
+          </div>
         </div>
-        <p className="main-text-size prose text-justify font-serif text-gray-700 lg:prose-xl dark:text-gray-300">
+        <p className="custom-prose text-pretty text-justify font-serif text-black dark:text-gray-300">
           Away from my keyboard, I love connecting with nature on sunny days, at
           the mountain or at the beach. On rainy days, I swap code for recipes,
           cooking for loved ones and creating connections around the table. I
-          also enjoy quiet moments with a book or a brush.
+          also enjoy quiet moments with a book or maybe a brush and riding my
+          bicycle through the city.
         </p>
       </div>
     </Card>
