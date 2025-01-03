9c187d97a25bb19e1330b686ff0d678180d0348c
Maria Marinescu <mariafmarinescu@outlook.com>
2025-01-03 21:13:32 +0100

    fix(redirect): resume link

--- src/modules/app/dashboard/dashboardCards/welcomeCard/ContactButtonGroup.tsx
+++ src/modules/app/dashboard/dashboardCards/welcomeCard/ContactButtonGroup.tsx
@@ -35,7 +35,7 @@ const getButtonListConfig = (isDarkThemeActive: boolean | undefined) => [
     ariaLabel: 'Access my Github profile',
   },
   {
-    href: 'https://drive.google.com/file/d/1jAJcdkcsiK1cOIWmreTzeKLIRRAWeKC-/view?usp=sharing',
+    href: 'https://drive.google.com/file/d/1fhYgNE696jqoJhkZdBN8WDev8umaP-LO/view?usp=sharing',
     label: 'Resume',
     className: buttonClassName,
     id: 'download-resume',
