2cdb7cd68e72b4c5c526282198b6d3ce2b72579f
Maria Marinescu <mariafmarinescu@outlook.com>
2025-01-12 20:55:52 +0100

    feat: document ThemeButton; add aria elements

--- src/components/buttons/ThemeButton.tsx
+++ src/components/buttons/ThemeButton.tsx
@@ -1,22 +1,36 @@
 import { ThemeAwareTooltip } from 'components/ThemeAwareTooltip';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { IoMdMoon, IoMdSunny } from 'react-icons/io';
 import { ToggleButton } from '.';
 
+/**
+ * ThemeButton component allows users to toggle between light and dark themes.
+ *
+ * This component uses a toggle button to switch themes and displays a tooltip
+ * indicating the current theme. It also ensures that the theme state is synchronized
+ * with the DOM.
+ *
+ * @returns {JSX.Element} The rendered ThemeButton component.
+ */
 export const ThemeButton: React.FC = () => {
-  const [isDarkActive, setIsDarkActive] = useState(false);
+  const [isDarkActive, setIsDarkActive] = useState<boolean>(() => {
+    return document.body.classList.contains('dark');
+  });
 
-  const toggleDarkMode = () => {
-    setIsDarkActive(true);
-    return document.body.classList.toggle('dark');
-  };
+  const toggleDarkMode = useCallback(() => {
+    setIsDarkActive((prev) => !prev);
+    document.body.classList.toggle('dark');
+  }, [isDarkActive]);
 
   return (
     <>
       <div
         className="flex h-fit w-fit"
-        data-tooltip-content={`Switch to ${!!isDarkActive ? 'light' : 'dark'} theme`}
+        data-tooltip-content={`Switch to ${isDarkActive ? 'light' : 'dark'} theme`}
         data-tooltip-id="theme-switch-button"
+        role="button"
+        aria-label={`Switch to ${isDarkActive ? 'light' : 'dark'} theme`}
+        aria-pressed={isDarkActive}
       >
         <ToggleButton
           activeIcon={<IoMdSunny size={20} className="text-yellow-800" />}
